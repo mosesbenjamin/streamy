952ba7ac5ca4ae6e65946710e5ca8429ecf720fe
El-Mavewrick <60680552+El-Mavewrick@users.noreply.github.com>
2020-06-10 22:44:16 +0100

    Adding server and client updates

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,7 @@ const App = ()=>{
 		 <div>
 		  <Header />
 		  <Route path="/" exact component={StreamList}  />
-		  <Route path="/streams/show" exact component={StreamShow}  />
+		  <Route path="/streams/:id" exact component={StreamShow}  />
 		  <Route path="/streams/new" exact component={StreamCreate}  />
 		  <Route path="/streams/edit/:id" exact component={StreamEdit}  />
 		  <Route path="/streams/delete/:id" exact component={StreamDelete}  />
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -15,7 +15,7 @@ class StreamList extends React.Component{
 					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
 						Edit
 					</Link>	
-					<Link to={ `/streams/delete/${stream.id}` } className="ui button negative">
+					<Link to={ `/streams/delete/${stream.id}`} className="ui button negative">
 						Delete
 					</Link>				
 				</div>
@@ -27,13 +27,15 @@ class StreamList extends React.Component{
 		return this.props.streams.map(stream=>{
 			return(
 				<div className="item" key={stream.id}>
-				{this.renderAdmin(stream)}
+					{this.renderAdmin(stream)}
 				 <i className="large middle aligned icon camera"/>
 				 <div className="content">
-				 	{stream.title}
-				 	<div className="description">
-				 	{stream.description}
-				 </div>
+					 <Link to={`/streams/${stream.id}`} className="header">
+					 	{stream.title}
+					 </Link>
+					 <div className="description">
+					 	{stream.description}
+					 </div>
 				 </div>
 				</div>
 				)
